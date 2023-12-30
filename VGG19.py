2f4c0d962c5fe12921fa60fea731e093e7064c34
Devendiran13 <115154306+Devendiran13@users.noreply.github.com>
2023-12-30 16:11:14 +0530

    VGG19.py

--- VGG19.py
+++ VGG19.py
@@ -1,1 +1,35 @@
+import os
+import cv2
+import pickle
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import keras
+import tensorflow
+from tensorflow.keras.models import Model, Sequential
+from tensorflow.keras.utils import plot_model
+from tensorflow.keras.applications import VGG19
+from tensorflow.keras.callbacks import EarlyStopping
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.layers import Input, Lambda, Dense, Flatten, Dropout, BatchNormalization, Activation
+from sklearn.metrics import confusion_matrix, classification_report, accuracy_score, recall_score, precision_score, f1_score
 
+vgg = VGG19(input_shape=(250, 250, 3), weights='imagenet', include_top=False)
+for layer in vgg.layers:
+    layer.trainable = False
+
+x = Flatten()(vgg.output)
+prediction = Dense(3, activation='softmax')(x)
+model = Model(inputs=vgg.input, outputs=prediction)
+
+model.compile(loss='sparse_categorical_crossentropy', optimizer="adam", metrics=['accuracy'])
+
+plot_model(model=model, show_shapes=True)
+
+early_stop = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=5)
+history = model.fit(x_train, y_train, validation_data=(x_val, y_val), epochs=10, callbacks=[early_stop], batch_size=30, shuffle=True)
+
+test_loss, test_accuracy = model.evaluate(x_test, y_test)
+print("Test Accuracy:", test_accuracy)
