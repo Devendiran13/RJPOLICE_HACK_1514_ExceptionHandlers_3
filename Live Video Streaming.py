55544dc23a7765ed3f6a5224a45af99aa83215a3
Devendiran13 <115154306+Devendiran13@users.noreply.github.com>
2023-12-30 16:20:24 +0530

    (RSTP Protocol) Live Video Streaming.py

--- Live Video Streaming.py
+++ Live Video Streaming.py
@@ -1,1 +1,19 @@
+import cv2
+
+rtsp_url = 'rtsp://your_stream_url'
+cap = cv2.VideoCapture(rtsp_url)
+
+if not cap.isOpened():
+    print("Cannot connect to the RTSP stream. Check the URL or stream availability.")
+    exit()
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        print("Error reading frame")
+        break
+    cv2.imshow('RTSP Stream', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()
 
